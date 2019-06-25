afee4ded940a82037d0acc261954855944c746cd
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-06-25 12:43:50 -0400

    retrive single selected post and display in seprate FullPost section with title and body of the post add componentDidUpdate to not send infite request once we have the data of particular id on page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,17 +9,18 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount(){
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then(response => {
-                const posts = response.data.slice(0,4);
+                const posts = response.data.slice(80,84);
                 const updatedPost = posts.map( post => {
                     return {
                         ...post,
-                        author: "Zakwan"
+                        author: "- Zakwan"
                     }
                 });
                 this.setState({posts: updatedPost});
@@ -27,10 +28,19 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
 
         const posts = this.state.posts.map( post => {
-            return <Post key = {post.id} title = {post.title} author = {post.author} />
+            return <Post 
+                        key = {post.id} 
+                        title = {post.title} 
+                        author = {post.author} 
+                        clicked = { () => this.postSelectedHandler(post.id)}
+                    />
         });
 
         return (
@@ -39,7 +49,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id = {this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
